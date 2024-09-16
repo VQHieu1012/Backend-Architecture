fa6b16c601fef306df40762c7856d216e8ad4bf9
VQHieu1012 <vuquanghieuaz@gmail.com>
2024-09-17 00:02:41 +0700

    API response using class

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,12 +1,16 @@
 'use strict'
 
-const AccessService = require("../services/access.service")
+const AccessService = require("../services/access.service");
+const {OK, CREATED} = require('../core/success.response');
 
 class AccessController {
 
     signUp = async ( req, res, next) => {
         console.log("Come to AccessController Signup")
-        return res.status(201).json(await AccessService.signUp(req.body))
+        new CREATED({
+            message: 'Registered OK!',
+            metadata: await AccessService.signUp(req.body)
+        }).send(res)
     }
 }
 
