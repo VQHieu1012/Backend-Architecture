e808aa5436b0ed2a3f329d5fecff9d561d9c7c66
VQHieu1012 <vuquanghieuaz@gmail.com>
2024-09-20 21:05:50 +0700

    create product model

---
+++ src/models/product.model.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const { Schema, model } = require('mongoose');
+
+const DOCUMENT_NAME = 'Product'
+const COLLECTION_NAME = 'Products'
+
+const productSchema = new Schema ({
+    product_name: {type: String, required: true},
+    product_thumb: {type: String, required: true},
+    product_description: String,
+    product_price: {type: Number, required: true},
+    product_quantity: {type: Number, required: true},
+    product_type: {type: String, required: true, enum: ['Electronics', 'Clothing', 'Furniture']},
+    product_attributes: {type: Schema.Types.Mixed, required: true}
+}, {
+    collection: COLLECTION_NAME,
+    timestamps: true
+})
+
+// define the product type = clothing
+
+const clothingSchema = new Schema ({
+    brand: {type: String, required: true},
+    size: String,
+    material: String
+}, {
+    collection: 'clothes',
+    timestamps: true
+})
+
+const electronicSchema = new Schema ({
+    manufacturer: {type: String, required: true},
+    model: String,
+    color: String
+}, {
+    collection: 'clothes',
+    timestamps: true
+})
+
+module.exports = {
+    product: model(DOCUMENT_NAME, productSchema),
+    electronic: model('Electronics', electronicSchema),
+    clothing: model('Clothing', clothingSchema)
+}
